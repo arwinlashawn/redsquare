b3baf5ff1a22c80f80710177cc2cbe6ee8b15e5b
arwinlashawn <aanakkendary@ucdavis.edu>
2020-08-05 17:12:42 +0800

    updated python answers

--- python.py
+++ python.py
@@ -4,22 +4,30 @@ countries = ["Malaysia", "Singapore", "Thailand", "Brunei", "Indonesia",
 "Japan", "China", "India", "South Korea", "Vietnam" ]
 
 def search(name):
-	if name in countries:
-		print(name)
+	if name.title() in countries:
+		print(name.title())
 	else:
 		print("Not Found")
 
+# uppercase
+search("vietnaM")
 
 ### 2 ###
+from abc import ABC, abstractmethod
 import math
 
-class Animal:
+class Animal(ABC):
 
 	x_coordinates = 0
 	y_coordinates = 0
 
-	def __init__(self):
-		raise TypeError("Not instantiable")
+# not optimal to make it abstract
+# abc meta
+# using abstactmethod to make Animal uninstantiable
+	@abstractmethod
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
 
 	# def __init__(self, x, y):
 	# 	self.x_coordinates = x
@@ -41,8 +49,7 @@ class Animal:
 class Dog(Animal):
 
 	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+		super().__init__(x, y)
 
 	def distance(self, cat):
 		return math.sqrt((self.x-cat.x) ** 2 + (self.y-cat.y) ** 2)
@@ -51,8 +58,7 @@ class Dog(Animal):
 class Cat(Animal):
 
 	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+		super().__init__(x, y)
 
 	def distance(self, dog):
 		return math.sqrt((self.x-dog.x) ** 2 + (self.y-dog.y) ** 2)
@@ -65,8 +71,10 @@ class Cat(Animal):
 #x = Animal() 
 
 # check distance output
-snoopy = Dog(-7,-4)
-garfield = Cat(17, 6.5)
+snoopy = Dog(0,3)
+garfield = Cat(4, 0)
+pussinboots = Cat(3,0)
 D = snoopy.distance(garfield)
+
 print(D) # maybe need to set to 6 decimal places?
- 
+
